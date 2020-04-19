cb81e524a4d9a22ca1a819b9ffd417e04784a9e8
Hwangstas <eugenehwang09@gmail.com>
2020-04-18 23:55:35 -0700

    Updated

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,44 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrixVar = matrix()) {
+        matrixInverse <- NULL
+        set <- function(y) {
+            if (is.null(y)) {
+                message("please set a non-null matrix")
+                return()
+            }
+            if (!identical(matrixVar, y)) {
+                message("setting new matrix")
+                matrixVar <<- y
+                matrixInverse <<- NULL
+            }
+        }
+        get <- function() matrixVar
+        setMatrixInverse <- function(inverse) matrixInverse <<- inverse
+        getMatrixInverse <- function() matrixInverse
+        list(set = set, get = get,
+             setMatrixInverse = setMatrixInverse,
+             getMatrixInverse = getMatrixInverse)
 }
-
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        if (is.null(x)) {
+            message("cache matrix not created")
+            return(NULL)
+        }
+
+        cachedMatrixInverse <- x$getMatrixInverse()
+        if(!is.null(cachedMatrixInverse)) {
+            message("getting cached data")
+            return(cachedMatrixInverse)
+        }
+        data <- x$get()
+        if (!is.null(data)) {
+            newMatrixInverse <- solve(data, ...)
+            x$setMatrixInverse(newMatrixInverse)
+            newMatrixInverse   
+        } else {
+            message("please set a non-null matrix")
+        }
 }
